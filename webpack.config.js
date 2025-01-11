095da9dc5e2fd9eec10933f3e1417066c3edca90
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-01-11 14:26:28 +0200

    plugin connected to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,49 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
-	entry: {
-		app: './src/assets/js/index.js'
-	},
-	output: {
-		clean: true,
-		filename: '[name].bundle.js',
-		publicPath: '/god-of-war-ragnarok/',
-		path: path.resolve(__dirname, 'dist')
-	},
-	mode: 'development',
-	devServer: {
-		static: './src',
-		compress: true,
-		port: 9000,
-		hot: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(s[ac]ss|css)$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource'
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'God of War',
-			template: 'src/index.html'
-		})
-	]
-}
+  entry: {
+    app: './src/assets/js/index.js',
+  },
+  output: {
+    clean: true,
+    filename: '[name].bundle.js',
+    publicPath: '/god-of-war-ragnarok/',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: './src',
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'God of War',
+      template: 'src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets/images'),
+          to: 'assets/images',
+        },
+      ],
+    }),
+  ],
+};
