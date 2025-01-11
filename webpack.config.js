302c0fe70c05b6d323c32713cb1b4323e78be869
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-01-12 00:17:35 +0200

    changed publicPath in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     clean: true,
     filename: '[name].bundle.js',
-    publicPath: '/god-of-war-ragnarok/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/god-of-war-ragnarok/' : '/',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
